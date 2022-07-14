48011c397ea5d2791c224918cd5ae06e944ceb24
Carlos Castillo <Carlos.Castillo@dalog.net>
2022-07-14 12:20:02 +0200

    Include both

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -8,14 +8,25 @@ function handleSubmit(event) {
     console.log("::: Form Submitted :::")
     Client.postData('http://localhost:8082/getAPI', {text: formText})
     .then(function(data) {
+
+        console.log(formText)
         console.log(data.geonames[0])
         console.log(data.geonames[0].name)
         console.log(data.geonames[0].countryName)
         console.log(data.geonames[0].countryCode)
         console.log(data.geonames[0].lat)
         console.log(data.geonames[0].lng)
-        console.log("change5")
+        console.log("change7")
+
+        formText = data.geonames[0].lat 
+
+        Client.postData('http://localhost:8082/getAPI2', {text: formText})
+        .then(function(data2) {
+            console.log(formText)
+            console.log(data2)
+        })
         /*
+
         document.getElementById('text').innerHTML = "Text: " + formText
         document.getElementById('results').innerHTML = "Subjectivity: " + data.subjectivity
         document.getElementById('polarity').innerHTML = "Polarity: " + data.score_tag
--- src/server/index.js
+++ src/server/index.js
@@ -37,17 +37,29 @@ app.listen(8082, function () {
 // make API request
 app.post('/getAPI', async function(req, res) {
     const text = req.body.text;
-
     //const url = "https://api.meaningcloud.com/sentiment-2.1?key="+ process.env.API_KEY + "&of=json&txt=" + text + "&model=general&lang=en"
-
     const url = "http://api.geonames.org/searchJSON?q=london&maxRows=10&username=carloscastilloml"
-
     //console.log(`Your process.env.API_Key is ${ process.env.API_KEY}`); test
     console.log(`This is the url: ${url}`);
-
     const data = await fetch(url);
     try {
         res.send(await data.json());
+    }
+    catch(error) {
+        console.log(`Error in server/index.js - API : ${error}`);
+    }
+})
+
+
+app.post('/getAPI2', async function(req, res) {
+    const text = req.body.text;
+    //const url = "https://api.meaningcloud.com/sentiment-2.1?key="+ process.env.API_KEY + "&of=json&txt=" + text + "&model=general&lang=en"
+    const url = "https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=4fbf33d8795c485aa7d4e0484f7e6102"
+    //console.log(`Your process.env.API_Key is ${ process.env.API_KEY}`); test
+    console.log(`This is the url: ${url}`);
+    const data2 = await fetch(url);
+    try {
+        res.send(await data2.json());
     }
     catch(error) {
         console.log(`Error in server/index.js - API : ${error}`);
